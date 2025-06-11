215a2ca0e1d05ce3f0fe27140febf8b5bbb34059
Zivotu <130804369+Zivotu@users.noreply.github.com>
2025-06-11 17:49:53 +0200

    Fix import error by removing obsolete LanguageSelector

--- src/components/Layout.tsx
+++ src/components/Layout.tsx
@@ -1,31 +1,16 @@
 import Navigation from './Navigation';
 import Footer from './Footer';
 import GoogleTranslateWidget from './GoogleTranslateWidget';
-import LanguageSelector from './LanguageSelector';
-import { useGoogleTranslate } from '@/hooks/use-google-translate';
-import { useEffect } from 'react';
 
 interface LayoutProps {
   children: React.ReactNode;
 }
 
 const Layout = ({ children }: LayoutProps) => {
-  const translateTo = useGoogleTranslate();
-
-  useEffect(() => {
-    const userLang = (navigator.language || '').substring(0, 2);
-    const first = !localStorage.getItem('conexaLangSet');
-    if (first && ['hr', 'en', 'de'].includes(userLang)) {
-      translateTo(userLang);
-      localStorage.setItem('conexaLangSet', '1');
-    }
-  }, [translateTo]);
 
   return (
     <div className="min-h-screen bg-white flex flex-col">
       <div className="self-end p-2">
-        <LanguageSelector onSelect={translateTo} />
-        <div id="google_translate_element" className="mt-2" />
         <GoogleTranslateWidget />
       </div>
       <Navigation />
